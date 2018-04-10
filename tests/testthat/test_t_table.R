c933ace25f2a55b363fc78b0c5fcdaae84d6dc87
Greg Harvey <gregsharvey@gmail.com>
2018-04-09 21:28:40 -0400

    change expect functions

--- tests/testthat/test_t_table.R
+++ tests/testthat/test_t_table.R
@@ -4,8 +4,8 @@ test_that("t value ", {
   
   t_value <- round(t_table(alpha = .10, df = 1, tail = 1), 3) #should be 3.078
   
-  expect_equal(t_value, is_a("numeric"))
-  expect_equal(t_value, equals(3.078))
+  expect_is(t_value, is_a("numeric"))
+  expect_equal(t_value, 3.078)
   
   
 })
