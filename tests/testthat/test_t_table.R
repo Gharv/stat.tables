0ddf9916ce15553a3211915ff9531df18ab21ac9
Greg Harvey <gregsharvey@gmail.com>
2018-04-09 21:34:52 -0400

    Update test_t_table.R

--- tests/testthat/test_t_table.R
+++ tests/testthat/test_t_table.R
@@ -4,7 +4,7 @@ test_that("t value ", {
   
   t_value <- round(t_table(alpha = .10, df = 1, tail = 1), 3) #should be 3.078
   
-  expect_is(t_value, is_a("numeric"))
+  expect_is(t_value, "numeric")
   expect_equal(t_value, 3.078)
   
   
